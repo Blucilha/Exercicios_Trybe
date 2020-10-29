bd03ed6b482951f4549ed8b06e557fb699daf1c0
Blucilha <blucilha.nascimento@gmail.com>
2020-10-28 23:00:48 -0300

    Juntando os valores de dois objetos

--- bloco_4/dia_4/exercicioDois.js
+++ bloco_4/dia_4/exercicioDois.js
@@ -1,27 +1,20 @@
-
-
-// Boas vindas//
 let info = {
     personagem: "Margarida",
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-  };
+    recorrente: "sim"
+};
 
-console.log(`Bem vinda, ${info.personagem}.`);
+ let infoPatinhas = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "sim"
+};
 
-//Inserindo uma nova chave e valor//
+//Somando os valores//
+  console.log(info.personagem + " " + "e" + " " + infoPatinhas.personagem);
+  console.log(info.origem + " " + "e" + " " + infoPatinhas.origem);
+  console.log(info.nota + " " + "e" + " " + infoPatinhas.nota);
+  console.log(info.recorrente);
 
-info.recorrente = "sim";
-console.log(info);
-
-//Percorrer as chaves usando o for in//
-
-for (let chaves in info){
-    console.log(chaves);
-}
-
-//Percorrer apenas os valores//
-
-for (let chaves in info){
-    console.log(info[chaves]);
-}
