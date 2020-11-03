19f9b0265da9a8def4a09d011a0e96dda793995c
Blucilha <blucilha.nascimento@gmail.com>
2020-11-03 16:05:01 -0300

    Localizaçao de elementos e alterar texto pelo id

---
+++ bloco_5/aula_2/atividadeAula2.js
@@ -0,0 +1,4 @@
+let pai = document.querySelector("#elementoOndeVoceEsta").parentNode;
+pai.style.color = "purple";
+
+document.getElementById("primeiroFilhoDoFilho").innerText = "Preciso de muita:";
