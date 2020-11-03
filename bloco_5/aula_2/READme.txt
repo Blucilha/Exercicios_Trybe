46e619ff93e9bc5fed25b9bee49b2556f35f027e
Blucilha <blucilha.nascimento@gmail.com>
2020-11-03 19:15:09 -0300

    Adiciona uma div filho da div main-content

---
+++ bloco_5/aula_2/READme.txt
@@ -0,0 +1,6 @@
+Esta atividade do bloco 5, dia 2, ensina a:
+* Acessar elementos pelo javascript;
+* criar elementos html pelo javascript;
+* Inserir elementos html pelo javascript;
+* Deletar elementos pelos javascript;
+*
--- bloco_5/aula_2/atividadeAula2.js
+++ bloco_5/aula_2/atividadeAula2.js
@@ -2,7 +2,17 @@ let corpo = document.body;
 // Adiciona titulo//
 let title = document.createElement("h1");
 corpo.appendChild(title);
+
 // Adiciona div e classe//
 let div = document.createElement("div");
-corpo.appendChild(div);
 div.className = "main-content"
+corpo.appendChild(div);
+
+
+// Adiciona uma div filho da div "main-content"//
+let localizar = document.querySelector(".main-content");
+let outraDiv = document.createElement("div");
+outraDiv.className = "center-content";
+localizar.appendChild(outraDiv);
+
+
