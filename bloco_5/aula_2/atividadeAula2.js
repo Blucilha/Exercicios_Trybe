65608dc4f8ae1310938bb57cd020c9872ad400de
Blucilha <blucilha.nascimento@gmail.com>
2020-11-03 18:37:25 -0300

    Atividade do bloco 5_2: adiciona titulo, div e classe da div pelo js

--- bloco_5/aula_2/atividadeAula2.js
+++ bloco_5/aula_2/atividadeAula2.js
@@ -1,4 +1,8 @@
-let pai = document.querySelector("#elementoOndeVoceEsta").parentNode;
-pai.style.color = "purple";
-
-document.getElementById("primeiroFilhoDoFilho").innerText = "Preciso de muita:";
+let corpo = document.body;
+// Adiciona titulo//
+let title = document.createElement("h1");
+corpo.appendChild(title);
+// Adiciona div e classe//
+let div = document.createElement("div");
+corpo.appendChild(div);
+div.className = "main-content"
