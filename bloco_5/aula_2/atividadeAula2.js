697c4a9c92b47019650e15b950d31b52b4577049
Blucilha <blucilha.nascimento@gmail.com>
2020-11-04 18:51:10 -0300

    Atividaeds da aula 2

--- bloco_5/aula_2/atividadeAula2.js
+++ bloco_5/aula_2/atividadeAula2.js
@@ -1,7 +1,9 @@
-let corpo = document.body;
 // Adiciona titulo//
+let corpo = document.body;
 let title = document.createElement("h1");
+title.className = "titulo"
 corpo.appendChild(title);
+title.innerText = "Exercício 5.2 - JavaScript DOM";
 
 // Adiciona div e classe//
 let div = document.createElement("div");
@@ -26,5 +28,34 @@ let irmaoDaOutraDiv = document.createElement("div");
 irmaoDaOutraDiv.className = "left-content";
 localizar.appendChild(irmaoDaOutraDiv);
 
+// Adiciona outra div filho da primeira div//
+let irmaoDireitoDaOutra = document.createElement("div");
+irmaoDireitoDaOutra.className = "right-content";
+localizar.appendChild(irmaoDireitoDaOutra);
 
- 
+// Adiciona imagem//
+let imagem = document.createElement("IMG");
+imagem.className = "small-image"
+imagem.src = "https://picsum.photos/200";
+irmaoDaOutraDiv.appendChild(imagem);
+
+// Adicionando lista não ordenada//
+let listaDeNumeros = document.createElement("ul");
+listaDeNumeros.className = "lista_numeros";
+irmaoDireitoDaOutra.appendChild(listaDeNumeros);
+
+// Criando variavel de localização para a lista//
+let listagemDeNumeros = document.querySelector(".lista_numeros");
+
+// Criando lista//
+let listaNaoOrdenada = [
+    "um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez"
+]
+// Associando cada item à lista//
+for (let index in listaNaoOrdenada){
+    let numeros = listaNaoOrdenada[index];
+    let naoEnumera = document.createElement("ul");
+    naoEnumera.innerText = numeros;
+
+    listagemDeNumeros.appendChild(naoEnumera);
+}
